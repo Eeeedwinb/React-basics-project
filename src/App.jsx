29517b897c0dbc60ee4c334af1c93edf48579bec
Eeeedwinb <edwinbergwerff@hotmail.com>
2024-11-30 14:35:56 +0100

    added design for recipepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Center, Heading } from "@chakra-ui/react";
+import { Box, Center, Heading } from "@chakra-ui/react";
 import { RecipeSearch } from "./components/RecipeSearch";
 import { RecipePage } from "./pages/RecipePage";
 
@@ -8,16 +8,16 @@ export const App = () => {
   const [userRecipe, setUserRecipe] = useState();
 
   return (
-    <Center className="app" flexDir={"column"} margin={6} gap={6}>
-      <Heading size="2xl" margin={2} color="white" textAlign={"center"}>
-        Edwins Recipe Checker
-      </Heading>
+    <Center className="app" flexDir={"column"} gap={6}>
       {userRecipe ? (
         <RecipePage choice={userRecipe} clickFn={setUserRecipe} />
       ) : (
-        <>
+        <Box margin={6}>
+          <Heading size="2xl" margin={2} color="white" textAlign={"center"}>
+            Edwins Recipe Checker
+          </Heading>
           <RecipeSearch clickFn={setUserRecipe} />
-        </>
+        </Box>
       )}
     </Center>
   );
--- src/pages/RecipePage.jsx
+++ src/pages/RecipePage.jsx
@@ -3,48 +3,167 @@ import {
   Button,
   Center,
   Flex,
-  Heading,
+  Grid,
   Image,
+  Tag,
   Text,
 } from "@chakra-ui/react";
 
 export const RecipePage = ({ choice, clickFn }) => {
-  // const { isOpen, onOpen, onClose } = useDisclosure();
-
   const nutrientsToFilter = ["PROCNT", "FAT", "CHOCDF", "CHOLE", "NA"];
 
   return (
-    <Center flexDir={`column`} gap={4}>
-      <Heading>Het recept:</Heading>
-      <Box>{choice.label}</Box>
-      <Image src={choice.image} w={100} h={100} borderRadius={"2xl"} />
-      <Box>Meal type: {choice.mealType}</Box>
-      <Box>Dish type: {choice.dishType}</Box>
-      <Box>Total cooking time:{choice.totalTime}</Box>
-      <Box>Diet:{choice.dietLabels}</Box>
-      <Box>Cautions: {choice.cautions}</Box>
-      <Box>Health labels: {choice.healthLabels}</Box>
-      <Box>Ingredients:{choice.ingredientLines}</Box>
-      <Box>Servings:{choice.yield}</Box>
-      <Box>
-        Total Nutrients:
-        <Text color="red.800" size="xl">
-          {choice.calories.toFixed(0)} Calories
-        </Text>
-        {nutrientsToFilter.map((label) => {
-          const nutrient = choice.totalNutrients[label];
-          return (
-            <Text color="red.800" size="xl" key={label}>
-              {nutrient.quantity.toFixed(0)} {nutrient.unit} {nutrient.label}
-            </Text>
-          );
-        })}
-      </Box>
+    <Center
+      flexDir={"column"}
+      bgColor={"white"}
+      w={{ base: "100%", lg: "70%" }}
+      h={{ base: "auto", lg: "full" }}
+      gap={2}
+      overflow="hidden"
+      textAlign="center"
+      borderBottomRadius={{ base: "auto", lg: "2%" }}
+    >
       <Flex>
         <Button onClick={() => clickFn()} variant="ghost">
           Go back
         </Button>
       </Flex>
+      <Image
+        src={choice.image}
+        w={"full"}
+        h={{ base: "14em", lg: "14em" }}
+        objectFit="cover"
+      />
+
+      <Text color="gray.600" textTransform="uppercase" fontSize={"xs"}>
+        {choice.mealType} | {choice.dishType}
+      </Text>
+      <Text
+        fontSize={{ base: "4xl", lg: "4xl" }}
+        fontWeight={"medium"}
+        ml={2}
+        mr={2}
+      >
+        {choice.label}
+      </Text>
+      <Grid
+        templateColumns={{ base: "1fr", lg: "repeat(2, 50%)" }}
+        textAlign={{ base: "auto", lg: "left" }}
+        m={4}
+        pl={{ base: 0, lg: 5 }}
+        pr={{ base: 0, lg: 5 }}
+      >
+        <Box>
+          <Box mb={2}>
+            <Text textAlign={"left"} as={"span"}>
+              Total cooking time:
+            </Text>{" "}
+            <Text textAlign={"left"} as={"span"} fontWeight={"medium"}>
+              {choice.totalTime} minutes
+            </Text>
+          </Box>
+          <Box mb={4}>
+            <Text as={"span"}>Servings:</Text>{" "}
+            <Text as={"span"} fontWeight={"medium"}>
+              {choice.yield}
+            </Text>
+          </Box>
+
+          <Box mb={4}>
+            <Text fontWeight={"medium"} mb={1}>
+              Ingredients:
+            </Text>
+            {choice.ingredientLines.map((label) => (
+              <Text textAlign={"left"} key={label} mb={1}>
+                {label}
+              </Text>
+            ))}
+          </Box>
+        </Box>
+        <Box>
+          <Box mb={4}>
+            <Flex flexDir={"row"}>
+              <Box>
+                <Text mb={1}>Health Labels:</Text>
+                {choice.healthLabels.map((label) => (
+                  <Tag
+                    bgColor="purple.100"
+                    textColor="purple.800"
+                    textTransform="uppercase"
+                    fontSize={{ base: "xs", lg: "sm" }}
+                    fontWeight={"semibold"}
+                    key={label}
+                    ml={2}
+                    mb={2}
+                  >
+                    {label}
+                  </Tag>
+                ))}
+              </Box>
+            </Flex>
+          </Box>
+          <Box mb={4}>
+            <Text mb={1}>Diet:</Text>
+            {choice.dietLabels.map((label) => (
+              <Tag
+                bgColor="green.100"
+                textColor="green.800"
+                textTransform="uppercase"
+                fontSize={"sm"}
+                fontWeight={"medium"}
+                key={label}
+                ml={2}
+                mb={2}
+              >
+                {label}
+              </Tag>
+            ))}
+          </Box>
+          <Box mb={4}>
+            <Text mb={1}>Cautions:</Text>
+            {choice.cautions.map((label) => (
+              <Tag
+                bgColor="red.100"
+                textColor="red.800"
+                textTransform="uppercase"
+                fontWeight={"medium"}
+                fontSize={"sm"}
+                key={label}
+                ml={2}
+                mb={2}
+              >
+                {label}
+              </Tag>
+            ))}
+          </Box>
+          <Box mb={4}>
+            <Text fontWeight={"medium"}>Total Nutrients:</Text>
+            <Grid
+              templateColumns={"repeat(2, 1fr)"}
+              templateRows={{ base: "repeat(2, auto)", lg: "repeat(2, 1fr)" }}
+              gap={2}
+            >
+              <Text>
+                {choice.calories.toFixed(0)}
+                <Text textTransform="uppercase" fontWeight={"medium"}>
+                  Calories
+                </Text>
+              </Text>
+              {nutrientsToFilter.map((label) => {
+                const nutrient = choice.totalNutrients[label];
+                return (
+                  <Text key={label}>
+                    {nutrient.quantity.toFixed(0)} {nutrient.unit}{" "}
+                    <Text textTransform="uppercase" fontWeight={"medium"}>
+                      {nutrient.label}
+                    </Text>
+                  </Text>
+                );
+              })}
+            </Grid>
+          </Box>
+        </Box>
+      </Grid>
     </Center>
   );
 };
